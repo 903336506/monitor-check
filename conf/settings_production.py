d9eb84cf57013509aef778ff0ae6a94097d0af7d
李文毅 <liwenyi@jiagouyun.com>
2017-07-08 17:26:12 +0800

    prod

--- conf/settings_production.py
+++ conf/settings_production.py
@@ -11,7 +11,7 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': 'test',                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'prod',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'test',                            # 你的数据库user
         'PASSWORD': 'test',                        # 你的数据库password
         'HOST': 'rm-2zezm24r7p176s6rqo.mysql.rds.aliyuncs.com',                   		   # 数据库HOST
